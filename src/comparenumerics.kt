8bac201dc05bbee1deafffd65255101164ed1b1b
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2019-05-17 18:40:29 +0500

    Comparing Numeric Program in Kotlin

---
+++ src/comparenumerics.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val numOne = 15
+    val numTwo = 15
+
+    val match = (numOne == numTwo)
+    println("Match = $match")
+
+//    val matchTwo = numOne.equals(numTwo)
+//    println("Match = $matchTwo|")
+
+    println("Comparison result = ${numOne.compareTo(numTwo)}")
+
+}
