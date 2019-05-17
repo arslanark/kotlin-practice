fbf3f730d46888dc232e75e3ebdd537a6b4f0ebc
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2019-05-17 22:42:16 +0500

    Conditional if else statement in Kotlin

---
+++ src/ifelsecondition.kt
@@ -0,0 +1,12 @@
+fun main() {
+    print("Enter a state abbreviation: ")
+    val state = readLine()
+
+    if (state == "CA") {
+        println("The capital is Sacramento")
+    } else if (state == "OR") {
+        println("The capital is Salem")
+    } else {
+        println("I don't know that state")
+    }
+}
