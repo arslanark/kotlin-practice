944362c8ed24428b9b32d2884865bf9c118df180
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2019-05-17 17:12:53 +0500

    Simple Variables Program in Kotlin

---
+++ src/simplevariables.kt
@@ -0,0 +1,14 @@
+// Top level variable
+val name = "Arslan"
+
+fun main() {
+    val valueOne : Double = 54.toDouble()
+    println("valueOne: $valueOne")
+
+    var valueTwo = 54
+    println("valueTwo: $valueTwo")
+    valueTwo++
+    println("incremented valueTwo: $valueTwo")
+
+    println("Username: ${name.toLowerCase()}")
+}
