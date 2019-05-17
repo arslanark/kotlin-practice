91085203a948001257c57f510333cfa486c69774
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2019-05-17 20:42:59 +0500

    Arithmetic Operation Program in Kotlin

---
+++ src/arithmeticoperations.kt
@@ -0,0 +1,23 @@
+fun main() {
+    var numOne = 15
+    val numTwo = 10
+
+    val sum = numOne + numTwo
+    println("Direct Operation: $sum")
+
+    val sumFunction = numOne.plus(numTwo)
+    println("Function Operation: $sumFunction")
+
+    val sub = numOne - numTwo
+    println("Direct Subtraction Result: $sub")
+
+    val subFunction = numOne.minus(numTwo)
+    println("Function Subtraction Result: $subFunction")
+
+    numOne++
+    println("After increment: $numOne")
+
+    val numThree = numOne.inc()
+    println("After increment function: $numThree")
+
+}
