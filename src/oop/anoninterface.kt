19242b8b57fefef69234fa5c61c3f76d7ceeaea3
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2019-05-19 19:36:45 +0500

    Anonymous Interface in Kotlin

---
+++ src/oop/anoninterface.kt
@@ -0,0 +1,27 @@
+package oop
+
+fun main() {
+
+    val stateful = StatefulClass(object : ClickListener {
+        override fun onClick(event: ClickEvent) {
+            println("Click at ${event.x}, ${event.y}")
+        }
+    })
+    println("Listener initialized")
+    stateful.clickMe(5, 18)
+    stateful.clickMe(45, 57)
+}
+
+class ClickEvent(val x: Int, val y: Int)
+
+interface ClickListener {
+    fun onClick(event: ClickEvent)
+}
+
+class StatefulClass(listener: ClickListener) {
+    private var clickListener: ClickListener? = listener
+
+    fun clickMe(x: Int, y: Int) {
+        clickListener?.onClick(ClickEvent(x, y))
+    }
+}
